830ccb55dd531f99fa06c794346f0755b724b111
Mete <60179489+MeteSamlioglu@users.noreply.github.com>
2024-10-02 09:15:35 +0300

    test is updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+// src/App.test.js
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Body Weight Loss Simulator title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Body Weight Loss Simulator/i);
+  expect(titleElement).toBeInTheDocument();
 });
